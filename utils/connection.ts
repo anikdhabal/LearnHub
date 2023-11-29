5d92af80093cd0cc52909000c0e41c117229e091
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2023-11-30 01:09:55 +0530

    Update connection.ts

--- utils/connection.ts
+++ utils/connection.ts
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 export default async function Connection() {
     
-    mongoose.connect("mongodb+srv://adhabal2002:3DRndpHWhUW0Xiai@cluster0.wupnjdm.mongodb.net/");
+    mongoose.connect("");
 }
